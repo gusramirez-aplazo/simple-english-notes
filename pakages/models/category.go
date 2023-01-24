ee2b412e2141e72c25ef063c6131bb21577fbcfe
Gus Ramírez <gustavo@aplazo.mx>
2023-01-24 17:15:26 -0600

    fix: model typos in types

--- pakages/models/category.go
+++ pakages/models/category.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Category struct {
 	gorm.Model
 
-	ID          uint   `gorm:"primaryKey; autoIncrement; not null; unique_index"`
-	Name        string `gorm:"not null; unique; var(100)"`
-	Description string `gorm:"string"`
+	ID          uint   `gorm:"primaryKey;autoIncrement;not null;unique_index"`
+	Name        string `gorm:"not null;unique;type:varchar(100)"`
+	Description string
 }
--- pakages/models/note.go
+++ pakages/models/note.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Note struct {
 	gorm.Model
 
-	ID    uint   `gorm:"primaryKey; autoIncrement; not null; unique_index"`
-	Title string `gorm:"var(100)"`
-	Text  string `gorm:"string"`
+	ID          uint   `gorm:"primaryKey; autoIncrement; not null; unique_index"`
+	Title       string `gorm:"type:varchar(100)"`
+	Description string
 }
